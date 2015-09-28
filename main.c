be9be7f316721545d74a402e832b748ebaf5564d
Rafael Marques <rafael.marques.rodrigues95@gmail.com>
2015-09-28 20:45:20 -0300

    now reading heuristics from the maps file

--- main.c
+++ main.c
@@ -6,10 +6,6 @@
 #include "backtrack.h"
 #include "maps.h"
 
-//utilizar a implentação de grafos em matrix ao inves de lista ?
-//backtracking tem muita operação de colocar valor em aresta e acessar vertices e arestas
-//e a implentação de matriz é mais rápida nesse quesit
-
 void printVector(int *vector, Map *map, int n){
 	int i;
 
@@ -18,29 +14,23 @@ void printVector(int *vector, Map *map, int n){
 	}
 }
 
-void checkResult(Graph *graph, int *vector){
-	int i;
+HEURISTIC initialize(Map **map, Graph **graph){
+    HEURISTIC flag;
+	char aux;
 
-	for(i = 0; i < graph -> size; i++){
-		ListNode *current = graph -> vector[i] -> head -> next;
+    (*map) = (Map *) malloc(sizeof(Map));
 
-		while(current != NULL){
-			if(vector[current -> src] == vector[current -> dest])
-				printf("ERRROOOOOOOOOOOOOOOOOOOOU %d %d\n", current -> src, current -> dest);
-			current = current -> next;
-		}
-	}
-}
-
-void initialize(Map **map, Graph **graph){
-	(*map) = (Map *) malloc(sizeof(Map));
-
-	scanf("%d\n", &((*map) -> size));
+	scanf("%d ", &((*map) -> size));
+    scanf("%c\n", &aux);
+    
+    flag = ((int) aux) - 97;
 
 	(*graph) = createGraph((*map) -> size);
 
 	readMap((*map));
 	transformMap((*map), (*graph));
+
+    return flag;
 }
 
 int main(int argc, char *argv[]){
@@ -50,20 +40,18 @@ int main(int argc, char *argv[]){
 	int assignments;
 	Map *map;
 	clock_t start, end;
+    HEURISTIC flag;
 
-	initialize(&map, &graph);
+    flag =	initialize(&map, &graph);
 
 	start = clock();
-	assignments = backtracking(graph, &vector, atoi(argv[1]));
+	assignments = backtracking(graph, &vector, flag);
 	end = clock();
 
-	//printVector(vector, map, graph -> size);
-    printf("Heuristic %d\n", atoi(argv[1]));
+    printVector(vector, map, graph -> size);
+    printf("Heuristic %d\n", flag);
     printf("Time consumed: %lf\n", (double) (end - start)/CLOCKS_PER_SEC);
-	printf("Assignments made: %d\n\n", assignments);
-
-	//FUNÇÃO SÓ PRA TESTAR OS RESULTADOS NAO ESQUECER DE TIRAR ELA DEPOIS 
-	checkResult(graph, vector);
+	printf("Assignments made: %d\n", assignments);
 
 	freeGraph(&graph);
 	freeMap(&map);
